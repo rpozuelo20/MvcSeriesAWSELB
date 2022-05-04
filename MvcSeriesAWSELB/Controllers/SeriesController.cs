1510c3083f1113d9325c1141aeabe54bd3c19e8b
rpozuelo20 <rubenpozuelomartinez@gmail.com>
2022-05-04 14:10:01 +0200

    Add project files.

---
+++ MvcSeriesAWSELB/Controllers/SeriesController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcSeriesAWSELB.Models;
+using MvcSeriesAWSELB.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcSeriesAWSELB.Controllers
+{
+    public class SeriesController : Controller
+    {
+        private RepositorySeries repo;
+        public SeriesController(RepositorySeries repo)
+        {
+            this.repo = repo;
+        }
+
+        public IActionResult Index()
+        {
+            List<Serie> series = this.repo.GetSeries();
+            return View(series);
+        }
+        public IActionResult Serie(int id)
+        {
+            Serie serie = this.repo.FindSerie(id);
+            return View(serie);
+        }
+    }
+}
---
+++ MvcSeriesAWSELB/Repositories/RepositorySeries.cs
@@ -0,0 +1,51 @@
+﻿using MvcSeriesAWSELB.Data;
+using MvcSeriesAWSELB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcSeriesAWSELB.Repositories
+{
+    public class RepositorySeries
+    {
+        private SeriesContext context;
+        public RepositorySeries(SeriesContext context)
+        {
+            this.context = context;
+        }
+
+        private int GetMaxIdSerie()
+        {
+            if (this.context.Series.Count() == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return this.context.Series.Max(x => x.IdSerie) + 1;
+            }
+        }
+        public List<Serie> GetSeries()
+        {
+            return this.context.Series.ToList();
+        }
+        public Serie FindSerie(int id)
+        {
+            return this.context.Series.SingleOrDefault(x => x.IdSerie == id);
+        }
+        public void InsertSerie(string nombre, string imagen, double puntuacion, int anyo)
+        {
+            Serie serie = new Serie
+            {
+                IdSerie = this.GetMaxIdSerie(),
+                Nombre = nombre,
+                Imagen = imagen,
+                Puntuacion = puntuacion,
+                Anyo = anyo
+            };
+            this.context.Series.Add(serie);
+            this.context.SaveChanges();
+        }
+    }
+}
